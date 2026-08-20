08844e4978e488b4367416b6aab732a7d7a383b6
ruidengzhong <137017382+ruidengzhong@users.noreply.github.com>
2026-08-20 19:42:54 +0000

    Deploying to gh-pages from @ ruidengzhong/ruidengzhong.github.io@2694954eac013136a9196222abcce78f6dae0e35 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "",
+    "data-repo": "ruidengzhong/ruidengzhong.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
